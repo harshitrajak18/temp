08583b042862efa6461262dee79663199fe0edb9
Harshit Rajak <106432378+harshitrajak18@users.noreply.github.com>
2025-04-08 15:38:34 +0530

    Add files via upload

---
+++ frontend major project/Major-Project-FrontEnd/src/utils.jsx
@@ -0,0 +1,17 @@
+// utils.js
+export const base64ToFile = (base64String, filename) => {
+  const arr = base64String.split(',');
+  const mime = arr[0].match(/:(.*?);/)[1];
+  const bstr = atob(arr[1]);
+  let n = bstr.length;
+  const u8arr = new Uint8Array(n);
+
+  while (n--) {
+    u8arr[n] = bstr.charCodeAt(n);
+  }
+
+  return new File([u8arr], `${filename}_profile.png`, { type: mime });
+};
+
+
+// utils.js
